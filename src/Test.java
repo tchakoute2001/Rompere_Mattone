d5714c5feaea0be3d90b0548de68089bc57335b1
tchakoute2001 <146494365+tchakoute2001@users.noreply.github.com>
2025-10-30 14:51:55 +0100

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,1 +1,16 @@
+package Tchakoute;
 
+import javax.swing.JFrame;
+
+public class Test {
+		
+	public static void main(String[] args) {
+	
+	    JFrame frame = new JFrame("BreakBrick");
+	    GamePanel panel = new GamePanel();
+	    frame.setContentPane(panel);
+	    frame.setSize(GameConstants.WINDOW_WIDTH, GameConstants.WINDOW_HEIGHT);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setVisible(true);
+	}
+}
